e01e433ca62614d665f616a4da600696d249d022
Italo M <italomarcos0010@gmail.com>
2020-09-10 11:18:55 -0300

    route returns null

--- src/app/controllers/PostcodeController.js
+++ src/app/controllers/PostcodeController.js
@@ -2,8 +2,14 @@ import Postcode from '../models/Postcode';
 
 class PostcodeController {
   async index(req, res) {
+    console.log('one');
     const { postcode } = req.params;
+    console.log('two');
+    console.log(postcode);
+
     const location = await Postcode.findByPk(postcode);
+
+    console.log('fuck');
 
     return res.json(location);
   }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import PostcodeController from './app/controllers/PostcodeController';
 
 const routes = new Router();
 
+routes.get('/', (req, res) => res.send('ok'));
 routes.get('/postcodes/:postcode', PostcodeController.index);
 
 export default routes;
